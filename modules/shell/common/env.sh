a502c92511936110998e99dba05b30824fc0da8e
FrauH0lle <roland.goers@posteo.de>
2025-04-14 10:56:08 +0200

    shell/common: Add tealdear
    * modules/shell/common/env.sh: Add `~/.local/bin` to PATH.
    
    * modules/shell/common/config.toml: Add tasks to install and maintain
      tealdear.

--- modules/shell/common/env.sh
+++ modules/shell/common/env.sh
@@ -4,3 +4,6 @@ export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
+
+# Add ~/.local/bin to PATH
+pathmunge "$XDG_BIN_HOME"
