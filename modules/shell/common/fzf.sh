9defa534d155dbfffe13e9cc79ae1b4793259d6c
FrauH0lle <roland.goers@posteo.de>
2026-03-07 10:45:49 +0100

    refactor: Adjust config for TPH-L25308

--- modules/shell/common/fzf.sh
+++ modules/shell/common/fzf.sh
@@ -1,44 +1,46 @@
-{{#if (eq DOD_DISTRIBUTION_NAME 'arch')}}
 if [ "$IS_ZSH" = "true" ]; then
-    source /usr/share/fzf/completion.zsh
-    source /usr/share/fzf/key-bindings.zsh
+  {{#if (eq DOD_DISTRIBUTION_NAME 'arch')}}
+  source /usr/share/fzf/completion.zsh
+  source /usr/share/fzf/key-bindings.zsh
+  {{/if}}
+  {{#if (eq DOD_DISTRIBUTION_NAME 'gentoo')}}
+  source /usr/share/zsh/site-functions/_fzf
+  source /usr/share/fzf/key-bindings.zsh
+  {{/if}}
+  {{#if (eq DOD_DISTRIBUTION_NAME 'ubuntu')}}
+  source /usr/share/doc/fzf/examples/completion.zsh
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
+  {{/if}}
+  {{#if (eq DOD_DISTRIBUTION_NAME 'fedora')}}
+  source /usr/share/fzf/shell/key-bindings.zsh
+  {{/if}}
 fi
 
 if [ "$IS_BASH" = "true" ]; then
-    source /usr/share/fzf/completion.bash
-    source /usr/share/fzf/key-bindings.bash
-fi
-{{/if}}
-{{#if (eq DOD_DISTRIBUTION_NAME 'gentoo')}}
-if [ "$IS_ZSH" = "true" ]; then
-    source /usr/share/zsh/site-functions/_fzf
-    source /usr/share/fzf/key-bindings.zsh
-fi
-
-if [ "$IS_BASH" = "true" ]; then
-    source /usr/share/bash-completion/completions/fzf
-    source /usr/share/fzf/key-bindings.bash
-fi
-{{/if}}
-{{#if (eq DOD_DISTRIBUTION_NAME 'ubuntu')}}
-if [ "$IS_ZSH" = "true" ]; then
-    source /usr/share/doc/fzf/examples/completion.zsh
-    source /usr/share/doc/fzf/examples/key-bindings.zsh
+  {{#if (eq DOD_DISTRIBUTION_NAME 'arch')}}
+  source /usr/share/fzf/completion.bash
+  source /usr/share/fzf/key-bindings.bash
+  {{/if}}
+  {{#if (eq DOD_DISTRIBUTION_NAME 'gentoo')}}
+  source /usr/share/bash-completion/completions/fzf
+  source /usr/share/fzf/key-bindings.bash
+  {{/if}}
+  {{#if (eq DOD_DISTRIBUTION_NAME 'ubuntu')}}
+  source /usr/share/doc/fzf/examples/key-bindings.bash
+  {{/if}}
+  {{#if (eq DOD_DISTRIBUTION_NAME 'fedora')}}
+  source /usr/share/fzf/shell/key-bindings.bash
+  {{/if}}
 fi
 
-if [ "$IS_BASH" = "true" ]; then
-    source /usr/share/doc/fzf/examples/key-bindings.bash
-fi
-{{/if}}
-
-{{#if (eq DOD_DISTRIBUTION_NAME 'gentoo')}}
+{{#if (or (eq DOD_DISTRIBUTION_NAME 'gentoo') (eq DOD_DISTRIBUTION_NAME 'fedora') (eq DOD_DISTRIBUTION_NAME 'arch'))}}
 export FZF_DEFAULT_COMMAND='fd -H -I'
 {{/if}}
 {{#if (eq DOD_DISTRIBUTION_NAME 'ubuntu')}}
 export FZF_DEFAULT_COMMAND='fdfind -H -I'
 {{/if}}
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-{{#if (or (eq DOD_DISTRIBUTION_NAME 'gentoo') (eq DOD_DISTRIBUTION_NAME 'arch'))}}
+{{#if (or (eq DOD_DISTRIBUTION_NAME 'gentoo') (eq DOD_DISTRIBUTION_NAME 'fedora') (eq DOD_DISTRIBUTION_NAME 'arch'))}}
 export FZF_CTRL_T_OPTS="--preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'"
 export FZF_ALT_C_COMMAND='fd -H -I --type d'
 export FZF_ALT_C_OPTS="--preview 'eza --long --tree --level=1 --color=auto --icons=auto --group-directories-first -- {}'"
