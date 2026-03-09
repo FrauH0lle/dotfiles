a1eb67b14ea733b5e4ec8bae1a9c034f3fd8b943
FrauH0lle <roland.goers@posteo.de>
2026-03-09 11:32:54 +0100

    refactor: Update TPH-L25308
    * hosts/TPH-L25308/fedora-container.toml (depends_on):
    * hosts/TPH-L25308/home/##dot##emacs.d/site-lisp/config.el (zenit-font)
    (zenit-theme, evil-split-window-below, org-directory, ess, langtool)
    (jinx, gptel):
    * hosts/TPH-L25308/home/##dot##emacs.d/site-lisp/init.el: (:completion):

---
+++ hosts/TPH-L25308/home/##dot##emacs.d/site-lisp/config.el
@@ -0,0 +1,52 @@
+;; site-lisp/config.el -*- lexical-binding: t; -*-
+
+;;
+;;; UI
+
+;;;; Set font
+
+(setq zenit-font (font-spec :family "Aporetic Serif Mono" :size 18 :weight 'regular)
+      zenit-serif-font "Aporetic Serif"
+      zenit-variable-pitch-font "Aporetic Sans")
+
+;;;; Set theme
+
+(setq zenit-theme 'doom-one)
+
+
+;;
+;;; Modules
+
+;;;; :editor evil
+
+;; Switch to the new window after splitting
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
+
+;;;; :emacs org
+
+(setq org-directory "~/Projekte/org/")
+
+;;;; :lang ess
+
+;; Mark flycheck lintr settings as safe
+(after! ess
+  (put 'flycheck-lintr-linters 'safe-local-variable #'stringp))
+
+;;;; :checkers grammar
+
+(after! langtool
+  (setq langtool-java-classpath
+        "/usr/share/languagetool:/usr/share/java/languagetool/*"))
+
+;;;; :checkers spell
+
+(after! jinx
+  (setq! jinx-languages "en_US de_DE"))
+
+;;;; :tools gptel
+
+(after! gptel
+  ;; Set model for 'commit-summary preset
+  (setf (alist-get 'commit-summary gptel--known-presets)
+        (zenit-plist-merge '(:backend "GLM" :model 'glm-4.5-air) (alist-get 'commit-summary gptel--known-presets))))
---
+++ hosts/TPH-L25308/home/##dot##emacs.d/site-lisp/init.el
@@ -0,0 +1,87 @@
+;; site-lisp/init.el -*- lexical-binding: t; no-byte-compile: t; -*-
+
+(modules!
+
+ :completion
+ vertico
+ (corfu +icons)
+
+ :ui
+ doom-dashboard
+ doom-theming
+ hl-todo
+ indent-guides
+ (ligatures +extra)
+ modeline
+ nav-flash
+ popup
+ transient-state
+ (vc-gutter +pretty)
+ workspaces
+ ;; zen
+
+ :checkers
+ grammar
+ spell
+ syntax
+ ;; (syntax +flymake)
+
+ :editor
+ evil
+ file-templates
+ fold
+ (format +lsp)
+ multiple-cursors
+ rotate-text
+ snippets
+ word-wrap
+
+ :emacs
+ electric
+ eshell
+ ibuffer
+ (org
+  +dragndrop
+  +jupyter
+  ;; +pandoc
+  +pretty
+  +present)
+ undo
+ vc
+
+ :tools
+ (eval +overlay)
+ magit
+ debugger
+ direnv
+ editorconfig
+ llm
+ (lookup +dictionary +docsets)
+ ;; (lsp +eglot +lsp-flymake)
+ lsp
+ ;; (lsp +lsp-flymake)
+ ;; pdf
+
+ :os
+ ;; arch-linux
+
+ :lang
+ ;; common-lisp
+ data
+ emacs-lisp
+ (ess +lsp +tree-sitter)
+ ;; (latex
+ ;;  +fold
+ ;;  +latexmk)
+ markdown
+ (python +lsp +tree-sitter)
+ ;; (rust +lsp +tree-sitter)
+ (sh +lsp +tree-sitter)
+
+ :config
+ (default +bindings +smartparens)
+ ;; (:if (not init-file-debug) compile)
+ compile
+ )
+
+
