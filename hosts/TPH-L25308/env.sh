7d0c8ade74827d2ea3688ee4b088a98155182b51
FrauH0lle <roland.goers@posteo.de>
2026-03-10 09:26:58 +0100

    fix(TPH-L25308): Correct BROWSER env var
    * hosts/TPH-L25308/env.sh:
    * hosts/TPH-L25308/fedora-launcher.sh:

--- hosts/TPH-L25308/env.sh
+++ hosts/TPH-L25308/env.sh
@@ -1,4 +1,4 @@
 {{#if (eq DOD_DISTRIBUTION_NAME 'fedora')}}
-export BROWSER="~/.local/bin/distrobox-host-exec"
+export BROWSER="$HOME/.local/bin/distrobox-host-exec wslview"
 export SSL_CERT_FILE=/etc/pki/ca-trust/source/anchors/curl-ca-bundle.crt
 {{/if}}
--- hosts/TPH-L25308/fedora-launcher.sh
+++ hosts/TPH-L25308/fedora-launcher.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [ $# -gt 0 ]; then
-  distrobox enter fedora-tph -- bash -c "exit" && \
+  distrobox enter fedora-tph -- bash -l -c "exit" && \
     env -u PATH -u XDG_BIN_HOME \
     distrobox enter --additional-flags "--env HOSTPATH=$PATH" fedora-tph  -nw -- bash -l -c "$*"
 else
-  distrobox enter fedora-tph -- bash -c "exit" && \
+  distrobox enter fedora-tph -- bash -l -c "exit" && \
     env -u PATH -u XDG_BIN_HOME \
     distrobox enter --additional-flags "--env HOSTPATH=$PATH" fedora-tph -nw -- bash -l
 fi
