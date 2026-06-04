568953e5222d1e69d357e12290b5341afe49fcdc
FrauH0lle <roland.goers@posteo.de>
2026-06-04 20:27:53 +0200

    yamato: Update Emacs conf + local packages
    * hosts/yamato/config.toml (packages): Add `power-profiles-daemon`.
    
    * hosts/yamato/home/##dot##emacs.d/site-lisp/packages.el (gptel)
    (mevedel): Add local package conf.

---
+++ hosts/yamato/home/##dot##emacs.d/site-lisp/packages.el
@@ -0,0 +1,19 @@
+;; ~/.emacs.d/site-lisp/packages.el -*- lexical-binding: t; -*-
+
+;; The place for your local package declaration.
+
+(package! gptel
+  :recipe (:local-repo "~/gptel"
+           ;; :build (:not compile)
+           )
+  :lockfile tools_llm)
+
+(package! mevedel
+  :recipe (:host github
+           :repo "FrauH0lle/mevedel"
+           :files ("*.el")
+           :protocol ssh
+           :local-repo "~/Projekte/mevedel"
+           :build (:not compile)
+           )
+  :lockfile tools_llm)
